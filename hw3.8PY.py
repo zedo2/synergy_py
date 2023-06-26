c4c33678347879027a2031e7746d43b5338be810
zedo2 <88265539+zedo2@users.noreply.github.com>
2023-06-26 21:12:24 +0300

    Add files via upload

---
+++ hw3.8PY.py
@@ -0,0 +1,41 @@
+#  Задание 1
+n = int(input())
+lst = []
+
+for i in range(n):
+    lst.append(int(input()))
+
+lst.reverse()
+
+for i in lst:
+    print(i, end=" ")
+
+#  Задание 2
+n = int(input())
+lst = list(map(int, input().split()))
+
+for i in range(n//2):
+    last = lst.pop()
+    lst.insert(i, last)
+
+for i in lst:
+    print(i, end=" ")
+
+
+#  Задание 3
+m = int(input())
+n = int(input())
+weights = [int(input()) for i in range(n)]
+weights.sort()
+
+i = 0
+j = n-1
+boats = 0
+
+while i <= j:
+    if weights[i] + weights[j] <= m:
+        i += 1
+    j -= 1
+    boats += 1
+
+print(boats)
